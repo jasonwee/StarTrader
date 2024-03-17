cfd6ed1311e29d3cb9a302b508fddd9ad1ec3814
Jason Wee <peichieh@gmail.com>
2024-03-17 15:26:58 +0800

    mujoco_py patch

---
+++ mujoco_py_patch/__init__.py
@@ -0,0 +1,15 @@
+import os
+from mujoco_py import config
+config.init()
+
+# Ok, everything's great! Do the actual work now.
+from . import mjcore
+from .mjviewer import MjViewer
+from .mjcore import MjModel
+from .mjcore import register_license
+import os
+import sys
+from .mjconstants import *
+from .platname_targdir import targdir
+
+register_license(config.key_path)
